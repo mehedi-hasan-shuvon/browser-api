df7937b9e766cde01aeb4f749ae17fcee9ddec37
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-05 16:10:42 +0600

    dom vs bom

--- explore.js
+++ explore.js
@@ -1,2 +1,11 @@
-const name=15;
-document.getElementById('name');
+const name = 15;
+const student = {
+    name: "jolil",
+    friend: "borsha",
+    job: "business",
+    makeMovie: function (name) {
+        console.log(name);
+    }
+}
+// document.getElementById('name');
+console.log(student.makeMovie("din rat"));
